cfbddb51d2c2075105e7de2383c4f499e6d48eaa
Kemalkeskin99 <dkdk999k@gmail.com>
2022-06-21 17:17:24 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void add(Car car)
+        {
+            if (car.CarName.Length>=2 && car.DailyPrice >= 0 )
+            {
+                _carDal.Add(car);   
+
+            }
+            else {
+                Console.WriteLine("Araba eklenemez....");
+            }
+        }
+    }
+}
